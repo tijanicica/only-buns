eb1936111bfedb6bce4ae320e4bd79fb360cddaf
tijanicica <tijanapetrovicns03@gmail.com>
2024-10-19 11:44:25 +0200

    initial commit

---
+++ onlybuns/onlybuns/src/main/java/com/project/onlybuns/OnlybunsApplication.java
@@ -0,0 +1,13 @@
+package com.project.onlybuns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlybunsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlybunsApplication.class, args);
+	}
+
+}
---
+++ onlybuns/onlybuns/src/test/java/com/project/onlybuns/OnlybunsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.onlybuns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlybunsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
